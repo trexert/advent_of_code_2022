82aa70f7702ddb99bb509b8004ac4dd9978d4a38
Sam Tarver <sam.tarver@metaswitch.com>
2022-12-01 14:06:22 +0000

    day 1

---
+++ src/bin/day1/main.rs
@@ -0,0 +1,37 @@
+use std::cmp::Reverse;
+
+type Int = u32;
+
+fn main() {
+    let inventories = include_str!("input.txt")
+        .split("\n\n")
+        .map(|inventory_str| {
+            inventory_str
+                .split("\n")
+                .map(|calories_str| calories_str.parse::<Int>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    println!("Part1: {}", part1(&inventories));
+    println!("Part2: {}", part2(&inventories));
+}
+
+fn part1(inventories: &Vec<Vec<Int>>) -> Int {
+    inventories
+        .iter()
+        .map(|inventory| inventory.iter().sum())
+        .max()
+        .unwrap()
+}
+
+fn part2(inventories: &Vec<Vec<Int>>) -> Int {
+    let mut inventory_sums: Vec<Int> = inventories
+        .iter()
+        .map(|inventory| inventory.iter().sum())
+        .collect();
+
+    inventory_sums.sort_by_key(|&inventory_sum| Reverse(inventory_sum));
+
+    inventory_sums.iter().take(3).sum()
+}
