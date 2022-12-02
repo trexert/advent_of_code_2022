4194aa2158f3cb50cb7393bde3529fd5fe31a608
Sam Tarver <sam.tarver@metaswitch.com>
2022-12-02 12:48:28 +0000

    day2 part 2

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -10,6 +10,7 @@ fn main() {
         .collect();
 
     println!("Part1: {}", part1(&strategy));
+    println!("Part2: {}", part2(&strategy));
 }
 
 fn part1(strategy: &Vec<(&str, &str)>) -> Int {
@@ -23,6 +24,21 @@ fn part1(strategy: &Vec<(&str, &str)>) -> Int {
         ("C", "X") => 7,
         ("C", "Y") => 2,
         ("C", "Z") => 6,
+        _ => panic!("Unexpected input"),
+    })
+}
+
+fn part2(strategy: &Vec<(&str, &str)>) -> Int {
+    score_strategy(strategy, |round| match round {
+        ("A", "X") => 3,
+        ("A", "Y") => 4,
+        ("A", "Z") => 8,
+        ("B", "X") => 1,
+        ("B", "Y") => 5,
+        ("B", "Z") => 9,
+        ("C", "X") => 2,
+        ("C", "Y") => 6,
+        ("C", "Z") => 7,
         _ => panic!("Unexpected input"),
     })
 }
