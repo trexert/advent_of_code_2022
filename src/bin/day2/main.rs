7ac57a3a33d9d68f7b635d831049f150ac5356d4
Sam Tarver <sam.tarver@metaswitch.com>
2022-12-02 12:45:15 +0000

    day2 part 1 2nd attempt

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -1,35 +1,4 @@
-use std::collections::HashMap;
-use RPSMove::{P, R, S};
-
 type Int = u16;
-
-#[derive(Clone, Copy, Debug)]
-enum RPSMove {
-    R,
-    P,
-    S,
-}
-
-struct Round {
-    my_move: RPSMove,
-    other_move: RPSMove,
-}
-
-impl Round {
-    fn score(&self) -> Int {
-        match (&self.my_move, &self.other_move) {
-            (R, R) => 4,
-            (R, P) => 1,
-            (R, S) => 7,
-            (P, R) => 8,
-            (P, P) => 5,
-            (P, S) => 2,
-            (S, R) => 3,
-            (S, P) => 9,
-            (S, S) => 6,
-        }
-    }
-}
 
 fn main() {
     let strategy: Vec<(&str, &str)> = include_str!("input.txt")
@@ -44,21 +13,20 @@ fn main() {
 }
 
 fn part1(strategy: &Vec<(&str, &str)>) -> Int {
-    let mapping = HashMap::from_iter(
-        [("A", R), ("B", P), ("C", S), ("X", R), ("Y", P), ("Z", S)].into_iter(),
-    );
-    score_strategy(strategy, &mapping)
+    score_strategy(strategy, |round| match round {
+        ("A", "X") => 4,
+        ("A", "Y") => 8,
+        ("A", "Z") => 3,
+        ("B", "X") => 1,
+        ("B", "Y") => 5,
+        ("B", "Z") => 9,
+        ("C", "X") => 7,
+        ("C", "Y") => 2,
+        ("C", "Z") => 6,
+        _ => panic!("Unexpected input"),
+    })
 }
 
-fn score_strategy(strategy: &Vec<(&str, &str)>, mapping: &HashMap<&str, RPSMove>) -> Int {
-    strategy
-        .iter()
-        .map(|(other_move_str, my_move_str)| {
-            Round {
-                my_move: mapping[my_move_str],
-                other_move: mapping[other_move_str],
-            }
-            .score()
-        })
-        .sum()
+fn score_strategy<F: Fn((&str, &str)) -> Int>(strategy: &Vec<(&str, &str)>, scoring: F) -> Int {
+    strategy.iter().map(|&round| scoring(round)).sum()
 }
