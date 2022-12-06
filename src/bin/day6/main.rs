80d35ecb7957ba8e0e09ef97d054673066b2e7fc
Sam Tarver <starver@hotmail.co.uk>
2022-12-06 12:28:27 +0000

    Day 6 part 2

--- src/bin/day6/main.rs
+++ src/bin/day6/main.rs
@@ -1,23 +1,22 @@
 fn main() {
     let signal: Vec<char> = include_str!("input.txt").chars().collect();
 
-    println!("Part1: {}", part1(&signal));
+    println!("Part1: {}", search_for_marker(&signal, 4));
+    println!("Part2: {}", search_for_marker(&signal, 14));
 }
 
-fn part1(signal: &Vec<char>) -> usize {
+fn search_for_marker(signal: &Vec<char>, marker_size: usize) -> usize {
     signal
-        .windows(SIZE)
+        .windows(marker_size)
         .enumerate()
         .filter(|&(_, packet)| check_for_marker(packet))
         .next()
         .unwrap()
-        .0 + SIZE
+        .0
+        + marker_size
 }
 
 fn check_for_marker(packet: &[char]) -> bool {
-    assert!(packet.len() == SIZE);
     println!("{:?}", packet);
-    (0..SIZE).all(|i| ((i + 1)..SIZE).all(|j| packet[i] != packet[j]))
+    (0..packet.len()).all(|i| ((i + 1)..packet.len()).all(|j| packet[i] != packet[j]))
 }
-
-const SIZE: usize = 4;
